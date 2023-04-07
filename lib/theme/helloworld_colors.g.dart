6bdf0af3057f6aaec9d08f628c6615026dd042cf
Parabeac-Cloud <admin@parabeac.com>
2023-04-07 04:37:44 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/helloworld_colors.g.dart
@@ -0,0 +1,12 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class HelloworldColors {
+
+static const Color blueish = Color(0xff5ea2b8);
+}
---
+++ lib/theme/helloworld_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class HelloworldTextStyles {
+
+static const TextStyle newish = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
